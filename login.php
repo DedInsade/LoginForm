92ca206c5054acb9c60f82ab3b30ff5fe5d7cc04
Pepa the Frog <57067576+DedInsade@users.noreply.github.com>
2020-02-24 13:14:13 +0700

    New changes

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+    if (isset($_POST['submit'])) {
+        $un=$_POST['username'];
+        $pw=$_POST['password'];
+        
+        if ($un=='username' && $pw=='password') {
+            header("location:main.html");
+            exit();
+        }
+        else {
+            echo "Invalid User/Password"; 
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,7 +22,7 @@
     <body>
         <div id="form">
             <h1>Login form</h1>
-            <form action="process.php" method='POST'>
+            <form method='POST'>
                 <p>
                     <label>Username: </label> 
                     <input type="text" name="username" require>
@@ -21,4 +36,3 @@
         </div>
     </body>
 </html>
-
